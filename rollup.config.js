3259dcfc683394db17291874484e920ecd2e8fc9
Abdón Rodríguez Davila <a@abdonrd.com>
2019-07-30 22:53:51 +0200

    Add missed license

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Abdón Rodríguez Davila and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE.txt
+ * file in the root directory of this source tree.
+ */
+
 import { createCompatibilityConfig } from '@open-wc/building-rollup';
 
 const configs = createCompatibilityConfig({
