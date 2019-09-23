70b5a435341334257a7de922870537e639a22f79
Abdón Rodríguez Davila <a@abdonrd.com>
2019-09-23 11:14:05 +0200

    Use the new app-toast element

--- src/register-service-worker.ts
+++ src/register-service-worker.ts
@@ -44,30 +44,14 @@ export class AppToast extends LitElement {
   }
 }
 
-// TODO: Update with a toast custom element
 const createUIPrompt = ({ onAccept }: { onAccept: Function }) => {
-  const toastElement = document.createElement('div');
-  toastElement.style.display = 'flex';
-  toastElement.style.padding = '1rem';
-  toastElement.style.backgroundColor = '#dddddd';
+  const toastElement = document.createElement('app-toast');
 
-  const messageElement = document.createElement('div');
-  messageElement.style.flexGrow = '1';
-  messageElement.innerText = 'New version available!';
-
-  const updateButton = document.createElement('button');
-  updateButton.innerText = 'Update';
-  const closeButton = document.createElement('button');
-  closeButton.innerText = 'Close';
-
-  toastElement.appendChild(messageElement);
-  toastElement.appendChild(updateButton);
-  toastElement.appendChild(closeButton);
-
-  updateButton.addEventListener('click', () => {
+  toastElement.addEventListener('on-accept', () => {
     onAccept();
   });
-  closeButton.addEventListener('click', () => {
+
+  toastElement.addEventListener('on-cancel', () => {
     toastElement.remove();
   });
 
