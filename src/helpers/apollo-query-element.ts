4382ad32268c1d305a5152b7909c71c9ec5470a6
Abdón Rodríguez Davila <a@abdonrd.com>
2019-09-23 22:01:09 +0200

    [wip] create apollo query element

---
+++ src/helpers/apollo-query-element.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) IBM and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE.txt
+ * file in the root directory of this source tree.
+ */
+
+import { LitElement, property } from 'lit-element';
+import { ApolloClient, NetworkStatus } from 'apollo-boost';
+import { GraphQLError } from 'graphql';
+
+import { client } from '../services/graphql';
+
+export class ApolloQueryElement extends LitElement {
+  public client: ApolloClient<unknown> = client;
+
+  @property({ type: Object })
+  public query?: any;
+
+  @property({ type: Object })
+  public data?: any;
+
+  @property({ type: Array })
+  public errors?: readonly GraphQLError[];
+
+  @property({ type: Boolean })
+  public loading = true;
+
+  @property({ type: Number })
+  public networkStatus?: NetworkStatus;
+
+  @property({ type: Boolean })
+  public stale?: boolean;
+
+  connectedCallback() {
+    if (super.connectedCallback) {
+      super.connectedCallback();
+    }
+
+    this.requestQuery();
+  }
+
+  public async requestQuery() {
+    try {
+      const queryResult = await client.query({ query: this.query });
+
+      this.data = queryResult.data;
+      this.errors = queryResult.errors;
+      this.loading = queryResult.loading;
+      this.networkStatus = queryResult.networkStatus;
+      this.stale = queryResult.stale;
+    } catch (error) {
+      // TODO
+      console.log(error);
+    }
+  }
+}
