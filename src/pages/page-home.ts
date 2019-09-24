26e24a6a10f71e9ce3744431db4aa65a0812ef0b
Abdón Rodríguez Davila <a@abdonrd.com>
2019-09-24 10:15:00 +0200

    [wip] GraphQL query example

--- src/pages/page-home.ts
+++ src/pages/page-home.ts
@@ -5,11 +5,24 @@
  * file in the root directory of this source tree.
  */
 
-import { LitElement, html, customElement } from 'lit-element';
+import { html, customElement, property } from 'lit-element';
+
+import { ApolloQueryElement } from '../helpers/apollo-query-element';
+import { gql } from '../services/graphql';
 
 @customElement('page-home')
-export class PageHome extends LitElement {
+export class PageHome extends ApolloQueryElement {
+  @property({ type: Object })
+  query = gql`
+    query {
+      users(limit: 10) {
+        username
+      }
+    }
+  `;
+
   protected render() {
+    // prettier-ignore
     return html`
       <section>
         <h1>Home</h1>
@@ -27,6 +40,18 @@ export class PageHome extends LitElement {
           cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
           minima!
         </p>
+
+        <h2>Users</h2>
+
+        ${this.loading ? html`
+          <div>Loading users...</div>
+        ` : !this.loading && this.data ? html`
+          <ul>
+            ${this.data.users.map((user: any) => html`
+              <li>${user.username}</li>
+            `)}
+          </ul>
+        ` : null}
       </section>
     `;
   }
