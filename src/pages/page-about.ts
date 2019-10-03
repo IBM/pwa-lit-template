085656841d8329d6958df77be52664febccb30df
David Garcia <david.garcia1@ibm.com>
2019-10-03 09:50:40 +0200

    Initial mutation setup

--- src/pages/page-about.ts
+++ src/pages/page-about.ts
@@ -5,22 +5,75 @@
  * file in the root directory of this source tree.
  */
 
-import { LitElement, html, customElement } from 'lit-element';
+import { html, customElement } from 'lit-element';
+
+import { QueryElement } from '../components/query-element';
+import { client, gql } from '../graphql-service';
 
 @customElement('page-about')
-export class PageAbout extends LitElement {
+export class PageAbout extends QueryElement {
+  mutation = gql`
+    mutation updateUserMutation($id: ID!, $username: String!) {
+      updateUser(input: { where: { id: $id }, data: { username: $username } }) {
+        user {
+          username
+        }
+      }
+    }
+  `;
+
+  constructor() {
+    super(
+      gql`
+        query {
+          user(id: 1) {
+            username
+          }
+        }
+      `
+    );
+  }
+
   protected render() {
     return html`
       <section>
         <h1>About</h1>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
-          delectus? Unde, sit. Fuga modi ea praesentium. Nemo dicta qui, magnam
-          cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
-          minima!
-        </p>
+        ${this.loading
+          ? html`
+              <div>Loading user with id 1...</div>
+            `
+          : !this.loading && this.data
+          ? html`
+              <ul>
+                <li>${this.data.user.username}</li>
+              </ul>
+            `
+          : null}
+
+        <form id="form" @submit="${this.updateUser}">
+          <label for="username">New username:</label>
+          <input id="username" name="username" type="text" />
+          <input type="submit" value="Modify" />
+        </form>
       </section>
     `;
+  }
+
+  protected async updateUser(e: Event) {
+    e.preventDefault();
+
+    const form =
+      this.shadowRoot &&
+      (this.shadowRoot.getElementById('form') as HTMLFormElement | undefined);
+    if (form) {
+      const formData = new FormData(form);
+      const username = formData.get('username');
+
+      await client.mutate({
+        mutation: this.mutation,
+        variables: { id: 1, username }
+      });
+    }
   }
 }
