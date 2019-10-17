aa54ef103a2c0e00b29ac682181e319fd7c49f36
Abdón Rodríguez Davila <a@abdonrd.com>
2019-10-17 22:35:08 +0800

    Use @open-wc/building-rollup to generate the service worker

--- rollup.config.js
+++ rollup.config.js
@@ -8,41 +8,58 @@
 import { createCompatibilityConfig } from '@open-wc/building-rollup';
 import copy from 'rollup-plugin-cpy';
 import replace from 'rollup-plugin-replace';
+import workbox from 'rollup-plugin-workbox';
 
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
 const DIST_PATH = 'dist/';
 
 const configs = createCompatibilityConfig({
   input: './index.html',
+  plugins: {
+    workbox: false
+  },
   extensions: ['.js', '.ts']
 });
 
-export default configs.map((config, index) => {
-  const defaultConfig = {
-    ...config,
-    plugins: [
-      ...config.plugins,
-      replace({
-        include: 'src/config/index.ts',
-        development: ENVIRONMENT
-      })
-    ]
-  };
-
-  // We only need to execute the copy task once
-  if (index === 0) {
-    return {
-      ...defaultConfig,
-      plugins: [
-        ...defaultConfig.plugins,
-        copy({
-          files: ['images/**/*', 'manifest.webmanifest'],
-          dest: DIST_PATH,
-          options: { parents: true }
-        })
-      ]
-    };
-  }
-
-  return defaultConfig;
+// Add plugins to both configs
+configs.forEach(config => {
+  config.plugins = [
+    ...config.plugins,
+    replace({
+      include: 'src/config/index.ts',
+      development: ENVIRONMENT
+    })
+  ];
 });
+
+// Add plugins to legacy config
+// Added the copy task here because we only need to execute it once
+configs[0] = {
+  ...configs[0],
+  plugins: [
+    ...configs[0].plugins,
+    copy({
+      files: ['images/**/*', 'manifest.webmanifest'],
+      dest: DIST_PATH,
+      options: { parents: true }
+    })
+  ]
+};
+
+// Add plugins to modern config
+configs[1] = {
+  ...configs[1],
+  plugins: [
+    ...configs[1].plugins,
+    workbox({
+      mode: 'generateSW',
+      render: ({ swDest, count, size }) => {
+        console.log(`The service worker was written to ${swDest}`);
+        console.log(`${count} files will be precached, totalling ${size} B.`);
+      },
+      workboxConfig: require('./workbox-config.js')
+    })
+  ]
+};
+
+export default configs;
