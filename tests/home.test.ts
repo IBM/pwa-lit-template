5d63bb6b67ec5734f132b7ed980cdc3aaf6498ff
David García <9059044+Tansito@users.noreply.github.com>
2020-10-27 12:12:59 +0100

    Changes tests title

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -1,5 +1,5 @@
 describe('Home test', () => {
-  it('should have MyApplication as title', async () => {
+  it('should have the right title', async () => {
     await page.goto('http://localhost:8000');
     const title = await page.title();
     await expect(title).toEqual('MyApplication');
