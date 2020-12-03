6c65f0e6920a773553ea7a96ec3dd0e02a121369
Abdón Rodríguez Davila <a@abdonrd.com>
2020-12-03 16:12:26 +0100

    Use default export for the configs

--- src/config/config.development.ts
+++ src/config/config.development.ts
@@ -5,6 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export const config = {
+export default {
   environment: 'development'
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { config as environmentConfig } from './config.development';
+import environmentConfig from './config.development';
 
 const sharedConfig = {
   name: 'MyApplication',
